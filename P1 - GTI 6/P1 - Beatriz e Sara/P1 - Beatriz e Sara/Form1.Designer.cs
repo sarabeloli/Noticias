4f149ce203692c7cd472b0871acb135141edaaa0
Sara Mello Beloli <sarambeloli@gmail.com>
2025-04-24 13:01:12 -0300

    Commit Noticias

---
+++ P1 - GTI 6/P1 - Beatriz e Sara/P1 - Beatriz e Sara/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace P1___Beatriz_e_Sara
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuPolitica = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuEconomia = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuGuerra = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuParcerias = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuSair = new System.Windows.Forms.ToolStripMenuItem();
+            this.grpInfo = new System.Windows.Forms.GroupBox();
+            this.lblConteudo = new System.Windows.Forms.Label();
+            this.picImagem = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblHora = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnNoticias = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.cmbPaises = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            this.grpInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuPolitica,
+            this.menuEconomia,
+            this.menuGuerra,
+            this.menuParcerias,
+            this.menuSair});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(1067, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuPolitica
+            // 
+            this.menuPolitica.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.menuPolitica.Name = "menuPolitica";
+            this.menuPolitica.Size = new System.Drawing.Size(71, 24);
+            this.menuPolitica.Text = "Politica";
+            this.menuPolitica.Click += new System.EventHandler(this.menuPolitica_Click);
+            // 
+            // menuEconomia
+            // 
+            this.menuEconomia.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.menuEconomia.Name = "menuEconomia";
+            this.menuEconomia.Size = new System.Drawing.Size(89, 24);
+            this.menuEconomia.Text = "Economia";
+            this.menuEconomia.Click += new System.EventHandler(this.menuEconomia_Click);
+            // 
+            // menuGuerra
+            // 
+            this.menuGuerra.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.menuGuerra.Name = "menuGuerra";
+            this.menuGuerra.Size = new System.Drawing.Size(67, 24);
+            this.menuGuerra.Text = "Guerra";
+            this.menuGuerra.Click += new System.EventHandler(this.menuGuerra_Click);
+            // 
+            // menuParcerias
+            // 
+            this.menuParcerias.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.menuParcerias.Name = "menuParcerias";
+            this.menuParcerias.Size = new System.Drawing.Size(81, 24);
+            this.menuParcerias.Text = "Parcerias";
+            this.menuParcerias.Click += new System.EventHandler(this.menuParcerias_Click);
+            // 
+            // menuSair
+            // 
+            this.menuSair.BackColor = System.Drawing.Color.IndianRed;
+            this.menuSair.Name = "menuSair";
+            this.menuSair.Size = new System.Drawing.Size(48, 24);
+            this.menuSair.Text = "Sair";
+            this.menuSair.Click += new System.EventHandler(this.menuSair_Click);
+            // 
+            // grpInfo
+            // 
+            this.grpInfo.Controls.Add(this.lblConteudo);
+            this.grpInfo.Location = new System.Drawing.Point(16, 44);
+            this.grpInfo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpInfo.Name = "grpInfo";
+            this.grpInfo.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grpInfo.Size = new System.Drawing.Size(1035, 288);
+            this.grpInfo.TabIndex = 1;
+            this.grpInfo.TabStop = false;
+            this.grpInfo.Text = "Informações";
+            // 
+            // lblConteudo
+            // 
+            this.lblConteudo.AutoSize = true;
+            this.lblConteudo.Location = new System.Drawing.Point(8, 33);
+            this.lblConteudo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblConteudo.Name = "lblConteudo";
+            this.lblConteudo.Size = new System.Drawing.Size(165, 16);
+            this.lblConteudo.TabIndex = 0;
+            this.lblConteudo.Text = "Selecione um tema acima.";
+            // 
+            // picImagem
+            // 
+            this.picImagem.Location = new System.Drawing.Point(384, 340);
+            this.picImagem.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.picImagem.Name = "picImagem";
+            this.picImagem.Size = new System.Drawing.Size(349, 170);
+            this.picImagem.TabIndex = 2;
+            this.picImagem.TabStop = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblHora});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 528);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(1067, 26);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblHora
+            // 
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(130, 20);
+            this.lblHora.Text = "Carregando hora...";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnNoticias
+            // 
+            this.btnNoticias.Location = new System.Drawing.Point(937, 497);
+            this.btnNoticias.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnNoticias.Name = "btnNoticias";
+            this.btnNoticias.Size = new System.Drawing.Size(113, 26);
+            this.btnNoticias.TabIndex = 4;
+            this.btnNoticias.Text = "Abrir Notícias";
+            this.btnNoticias.UseVisualStyleBackColor = true;
+            this.btnNoticias.Click += new System.EventHandler(this.btnNoticias_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(16, 495);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(283, 22);
+            this.dateTimePicker1.TabIndex = 5;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // cmbPaises
+            // 
+            this.cmbPaises.FormattingEnabled = true;
+            this.cmbPaises.Items.AddRange(new object[] {
+            "EUA",
+            "Ucrânia",
+            "Rússia"});
+            this.cmbPaises.Location = new System.Drawing.Point(889, 4);
+            this.cmbPaises.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbPaises.Name = "cmbPaises";
+            this.cmbPaises.Size = new System.Drawing.Size(160, 24);
+            this.cmbPaises.TabIndex = 6;
+            this.cmbPaises.SelectedIndexChanged += new System.EventHandler(this.cmbPaises_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.cmbPaises);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnNoticias);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.picImagem);
+            this.Controls.Add(this.grpInfo);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "NoticiasBiaSa";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.grpInfo.ResumeLayout(false);
+            this.grpInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picImagem)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuPolitica;
+        private System.Windows.Forms.ToolStripMenuItem menuEconomia;
+        private System.Windows.Forms.ToolStripMenuItem menuGuerra;
+        private System.Windows.Forms.ToolStripMenuItem menuParcerias;
+        private System.Windows.Forms.ToolStripMenuItem menuSair;
+        private System.Windows.Forms.GroupBox grpInfo;
+        private System.Windows.Forms.Label lblConteudo;
+        private System.Windows.Forms.PictureBox picImagem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblHora;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnNoticias;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox cmbPaises;
+    }
+}
+
---
+++ P1 - GTI 6/P1 - Beatriz e Sara/P1 - Beatriz e Sara/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P1___Beatriz_e_Sara
+{
+    public partial class Form1 : Form
+    {
+        private string paisSelecionado = "EUA"; // Valor padrão
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start(); // Inicia o relógio
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AtualizarHora(); // Atualiza a hora ao abrir
+            cmbPaises.Items.AddRange(new string[] { "EUA", "Ucrânia", "Rússia" });
+            cmbPaises.SelectedIndex = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            AtualizarHora();
+        }
+
+        private void AtualizarHora()
+        {
+            lblHora.Text = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+        }
+
+        private void menuPolitica_Click(object sender, EventArgs e)
+        {
+            string texto = "";
+
+            switch (paisSelecionado)
+            {
+                case "EUA":
+                    texto = "Política dos EUA:\n\n\nEm abril de 2025, os Estados Unidos enfrentam um cenário político complexo.\n\n\nA administração do presidente Donald Trump tem adotado uma postura mais isolacionista, especialmente em relação ao conflito na Ucrânia.\n\n\nRecentemente, Trump suspendeu a assistência militar e o compartilhamento de inteligência com Kiev,\n\n\n após uma reunião tensa com o presidente ucraniano Volodymyr Zelensky na Casa Branca.\n\n\nEssa decisão gerou críticas de aliados europeus e levantou preocupações sobre o compromisso dos EUA com a segurança internacional.";
+                    break;
+                case "Ucrânia":
+                    texto = "Política da Ucrânia:\n\n\nA Ucrânia enfrenta desafios políticos significativos em 2025.\n\n\nO presidente Volodymyr Zelensky busca manter a unidade nacional e o apoio internacional, apesar das pressões internas e externas.\n\n\nA suspensão do apoio militar dos EUA complicou ainda mais a situação,\n\n\n levando o governo ucraniano a intensificar esforços diplomáticos com outros parceiros internacionais.";
+                    break;
+                case "Rússia":
+                    texto = "Política da Rússia:\n\n\nSob a liderança de Vladimir Putin, a Rússia mantém uma postura autoritária e centralizada.\n\n\nO governo tem promovido políticas que reforçam o controle estatal e limitam a oposição interna.\n\n\nRecentemente, Putin desafiou os EUA para um \"duelo de alta tecnologia\", destacando os avanços militares russos e a disposição para confrontos estratégicos.";
+                    break;
+            }
+
+            lblConteudo.Text = texto;
+            string nomeArquivo = $"{paisSelecionado}_politica.jpg";
+            CarregarImagemRedimensionada(nomeArquivo);
+        }
+
+        private void menuEconomia_Click(object sender, EventArgs e)
+        {
+            string texto = "";
+
+            switch (paisSelecionado)
+            {
+                case "EUA":
+                    texto = "Economia dos EUA:\n\n\nA economia americana continua robusta, com crescimento impulsionado por investimentos em tecnologia e energia.\n\n\nNo entanto, a reestruturação do capitalismo interno tem causado debates sobre desigualdade e políticas fiscais.\n\n\nO governo Trump tem promovido reformas que visam reduzir a intervenção estatal, que tem gerado tanto apoio quanto críticas de diferentes setores da sociedade.";
+                    break;
+                case "Ucrânia":
+                    texto = "Economia da Ucrânia:\n\n\nA economia ucraniana está sob forte pressão devido à guerra prolongada.\n\n\nA infraestrutura do país sofreu danos significativos, e a dependência de ajuda externa aumentou.\n\n\nRecentemente, a Ucrânia iniciou negociações com os EUA para um novo acordo de minerais,\n\n\n visando impulsionar a recuperação econômica e atrair investimentos estrangeiros.";
+                    break;
+                case "Rússia":
+                    texto = "Economia da Rússia:\n\n\nA economia russa enfrenta sanções internacionais, mas tem mostrado resiliência.\n\n\nO país tem buscado fortalecer laços econômicos com nações como China e Irã, além de investir em setores estratégicos para reduzir a dependência de mercados ocidentais.\n\n\nEssas medidas visam mitigar os impactos das sanções e manter a estabilidade econômica interna.";
+                    break;
+            }
+
+            lblConteudo.Text = texto;
+            string nomeArquivo = $"{paisSelecionado}_economia.jpg";
+            CarregarImagemRedimensionada(nomeArquivo);
+        }
+
+
+        private void menuGuerra_Click(object sender, EventArgs e)
+        {
+            string texto = "";
+
+            switch (paisSelecionado)
+            {
+                case "EUA":
+                    texto = "Conflito EUA-Ucrânia:\n\n\nOs Estados Unidos têm adotado uma abordagem mais cautelosa em relação ao conflito na Ucrânia.\n\n\nEmbora tenham suspendido parte do apoio militar, continuam envolvidos em negociações diplomáticas com a Rússia.\n\n\nRecentemente, representantes dos EUA e da Rússia se reuniram em Istambul para discutir a reabertura de embaixadas e missões diplomáticas,\n\n\n sinalizando uma tentativa de melhorar as relações bilaterais.";
+                    break;
+                case "Ucrânia":
+                    texto = "Guerra na Ucrânia:\n\n\nA guerra na Ucrânia continua intensa, com a Rússia controlando cerca de 19% do território ucraniano.\n\n\nAs forças ucranianas enfrentam desafios como escassez de recursos e moral abalada.\n\n\nApesar disso, o país mantém sua resistência e busca apoio internacional para fortalecer sua defesa e recuperar os territórios ocupados.";
+                    break;
+                case "Rússia":
+                    texto = "Conflito Rússia-Ucrânia:\n\n\nA Rússia continua sua ofensiva na Ucrânia, controlando uma parte significativa do território ucraniano.\n\n\nAs forças russas têm realizado ataques constantes, visando enfraquecer a resistência ucraniana.\n\n\nApesar das críticas internacionais, o governo russo mantém sua justificativa de que as ações são necessárias para proteger seus interesses e segurança nacional.";
+                    break;
+            }
+
+            lblConteudo.Text = texto;
+            string nomeArquivo = $"{paisSelecionado}_guerra.jpg";
+            CarregarImagemRedimensionada(nomeArquivo);
+        }
+
+
+        private void menuParcerias_Click(object sender, EventArgs e)
+        {
+            string texto = "";
+
+            switch (paisSelecionado)
+            {
+                case "EUA":
+                    texto = "Parcerias dos EUA:\n\n\nApesar das tensões, os EUA mantêm parcerias estratégicas com diversos países.\n\n\nA relação com a Ucrânia, embora abalada, ainda inclui acordos em áreas como minerais e tecnologia.\n\n\nAlém disso, os EUA continuam a colaborar com aliados europeus e asiáticos em questões de segurança e comércio, buscando equilibrar seus interesses globais.";
+                    break;
+                case "Ucrânia":
+                    texto = "Parcerias da Ucrânia:\n\n\nA Ucrânia tem fortalecido suas parcerias com países europeus e outras nações aliadas.\n\n\nApesar das recentes tensões com os EUA, o país continua a buscar acordos estratégicos que possam contribuir para sua segurança e reconstrução.\n\n\nA cooperação com a União Europeia e outras organizações internacionais permanece vital para o futuro da Ucrânia.";
+                    break;
+                case "Rússia":
+                    texto = "Parcerias da Rússia:\n\n\nA Rússia tem fortalecido suas parcerias com países que compartilham interesses estratégicos semelhantes.\n\n\nA colaboração com nações como China e Irã tem se intensificado, abrangendo áreas como comércio, energia e defesa.\n\n\nEssas alianças visam contrabalançar o isolamento imposto pelas sanções ocidentais e expandir a influência russa em outras regiões.";
+                    break;
+            }
+
+            lblConteudo.Text = texto;
+            string nomeArquivo = $"{paisSelecionado}_parcerias.jpg";
+            CarregarImagemRedimensionada(nomeArquivo);
+        }
+
+        private void menuSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnNoticias_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.bbc.com/portuguese");
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("Data selecionada: " + dateTimePicker1.Value.ToShortDateString());
+        }
+
+        private void cmbPaises_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            paisSelecionado = cmbPaises.SelectedItem.ToString();
+            lblConteudo.Text = $"Selecione um tema para ver informações sobre {paisSelecionado}.";
+            picImagem.Image = null;
+        }
+
+        private Image ResizeImage(Image image, int width, int height)
+        {
+            Bitmap bmp = new Bitmap(width, height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.DrawImage(image, 0, 0, width, height);
+            }
+            return bmp;
+        }
+
+        private void CarregarImagemRedimensionada(string nomeArquivo)
+        {
+            string caminho = System.IO.Path.Combine(Application.StartupPath, nomeArquivo);
+
+            if (System.IO.File.Exists(caminho))
+            {
+                Image original = Image.FromFile(caminho);
+                Image redimensionada = ResizeImage(original, picImagem.Width, picImagem.Height);
+                picImagem.Image = redimensionada;
+            }
+            else
+            {
+                picImagem.Image = null;
+                MessageBox.Show("Imagem não encontrada: " + nomeArquivo);
+            }
+        }
+    }
+}
